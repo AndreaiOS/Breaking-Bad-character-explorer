cc8925a0ac66533235ccad692859a1a2606d7440
Andymurru <andrea.murru@tigerspike.com>
2020-09-08 10:32:53 +0100

    First commit

---
+++ Breaking-Bad-character-explorer/Breaking-Bad-character-explorer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Breaking-Bad-character-explorer
+//
+//  Created by Andrea Murru on 08/09/2020.
+//  Copyright © 2020 Andrea. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
